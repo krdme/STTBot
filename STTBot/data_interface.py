755f212f7dbfc5f288fb50ed51bf6a88b0570986
KeithDonaldson <krd@live.co.uk>
2021-06-12 17:02:28 +0100

    Test

--- STTBot/data_interface.py
+++ STTBot/data_interface.py
@@ -31,8 +31,7 @@ def get_pin(channel, timestamp):
 def get_random_pin(channel=None):
     cur = get_cur()
     if channel is not None:
-        env.log.info(f"Getting pin from {channel}")
-        cur.execute(Query.GET_RANDOM_PIN_FROM_CHANNEL, (channel))
+        cur.execute(Query.GET_RANDOM_PIN_FROM_CHANNEL, [channel])
     else:
         cur.execute(Query.GET_RANDOM_PIN)
     row = cur.fetchone()
