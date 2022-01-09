f70b1158e277b86f056468fb101b1368685a407e
dont-understood <81417188+dont-understood@users.noreply.github.com>
2022-01-09 21:05:15 +0100

    Make msg match command not tag

--- STTBot/events/app_mention.py
+++ STTBot/events/app_mention.py
@@ -214,6 +214,9 @@ def _cmd_msg_leaderboard(client, event_data, command, say):
         if leaderboard is None:
             raise CommandError(f"No matches found for {display_search_string}")
 
+        for k in leaderboard.keys():
+            leaderboard[k].replace("@", "#")
+
         longest_string = max(len(k) for k in leaderboard.keys()) + 2
         leader_str = '\n'.join([f'{k.ljust(longest_string)} {v}' for k, v in leaderboard.items()])
         message = f"""```{"User".ljust(longest_string)} Count of {display_search_string}\n{leader_str}```"""
